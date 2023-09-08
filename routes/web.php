723fa16679fdf9e60fb9b5c23e9b6d1828077d91
Cmadalinaandreea <cma.madalina.andreea@gmail.com>
2023-09-08 04:51:48 +0300

    commit crud updates

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blog', [\App\Http\Controllers\BlogPostController::class, 'index']);
+
+
+
+Route::get('/blog/{blogPost}', [\App\Http\Controllers\BlogPostController::class, 'show']);
+
+Route::get('/blog/create/post', [\App\Http\Controllers\BlogPostController::class, 'create']);
+Route::post('/blog/create/post', [\App\Http\Controllers\BlogPostController::class, 'store']);
+
+Route::get('/blog/{blogPost}/edit', [\App\Http\Controllers\BlogPostController::class, 'edit']);
+Route::put('/blog/{blogPost}/edit', [\App\Http\Controllers\BlogPostController::class, 'update']);
+
+Route::delete('/blog/{blogPost}', [\App\Http\Controllers\BlogPostController::class, 'destroy']);
